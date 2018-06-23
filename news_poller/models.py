040bb4ef94eaea1c55174babf69300d940d51f3a
Vishwas Mittal <vishwas.mittal78@gmail.com>
2018-06-23 10:53:15 +0530

    added rss news fetcher
    (cherry picked from commit 0fd550791de75ce06f44a4e9227a9641432d91fd)

---
+++ news_poller/models.py
@@ -0,0 +1,64 @@
+import datetime
+from pprint import pprint
+from marshmallow import Schema, fields, pre_load, post_load, pre_dump
+
+
+class NewsItem(object):
+    def __init__(self, title, summary, pub_date=datetime.datetime.now(), link="http://bl.ah", category=None,
+                 source=None):
+        self.source = source
+        self.title = title
+        self.summary = summary
+        self.category = category
+        self.pub_date = pub_date
+        self.link = link
+
+
+class RSS(object):
+    def __init__(self, title, items, language='en', link="http://bit.ly", subtitle="fast_news"):
+        self.language = language
+        self.title = title
+        self.link = link
+        self.subtitle = subtitle
+        self.items = items
+
+
+class RSSSchema(Schema):
+    language = fields.Str(data_key='language')
+    title = fields.Str(data_key='title')
+    link = fields.Url(data_key='link')
+    subtitle = fields.Str(data_key='subtitle')
+    items = fields.Nested('NewsItemSchema', exclude=('source',), many=True, data_key='entries')
+
+    @pre_load
+    def get_corr_data(self, data):
+        corr_data = data['feed']
+        corr_data['entries'] = data['entries']
+        return corr_data
+
+    @post_load
+    def make_feed(self, data):
+        return RSS(**data)
+
+
+class NewsItemSchema(Schema):
+    title = fields.Str(data_key='title')
+    summary = fields.Str(data_key='summary')
+    category = fields.Str(required=False, data_key='category')
+    # pub_date = fields.DateTime(format='rfc822', data_key='published')
+    link = fields.Url(data_key='link')
+
+    @post_load
+    def make_feed(self, data):
+        return NewsItem(**data)
+
+    @pre_dump
+    def get_date_format(self, data):
+        print(data['published'])
+        return
+
+
+# if __name__ == "__main__":
+#     rSch = RSSSchema().loads(open('sample_json_feed.json').read())
+#     pprint(RSSSchema().dumps(rSch, indent=4))
+#     # print(RSSSchema().dumps(rSch, indent=4))
